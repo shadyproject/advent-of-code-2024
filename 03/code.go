48f964f8ca144a05aa839454171974b9625567a7
Chris Martin <shady@shadyproject.net>
2024-12-03 09:53:46 -0600

    Day 3 part 2

--- 03/code.go
+++ 03/code.go
@@ -5,13 +5,15 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/herrnan/aoc-helper"
 )
 
 func main() {
-	// h, _ := aoc.NewHelper(3, 2024)
-	// input, _ := h.GetInput()
+	h, _ := aoc.NewHelper(3, 2024)
+	input, _ := h.GetInput()
 	// input := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-	input := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+	// input := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 	fmt.Print("Day 3\n")
 	fmt.Printf("Part 1 Answer: %v\n", Part1(input))
@@ -19,24 +21,44 @@ func main() {
 }
 
 func Part1(input string) int {
-
 	re := regexp.MustCompile(`mul\([0-9]{1,3},[0-9]{1,3}\)`)
 
 	sum := 0
 
 	for _, inst := range re.FindAllString(input, -1) {
-		// fmt.Printf("%v\n", inst)
-		idx := strings.Index(inst, ",")
-		end := strings.Index(inst, ")")
-		lhs, _ := strconv.Atoi(inst[4:idx])
-		rhs, _ := strconv.Atoi(inst[idx+1 : end])
-
-		sum += (lhs * rhs)
+		sum += DoMultInstruction(inst)
 	}
 
 	return sum
 }
 
+func DoMultInstruction(inst string) int {
+	idx := strings.Index(inst, ",")
+	end := strings.Index(inst, ")")
+	lhs, _ := strconv.Atoi(inst[4:idx])
+	rhs, _ := strconv.Atoi(inst[idx+1 : end])
+	return lhs * rhs
+}
+
 func Part2(input string) int {
-	return 0
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\([0-9]{1,3},[0-9]{1,3}\)`)
+
+	shouldSum := true
+	sum := 0
+
+	for _, inst := range re.FindAllString(input, -1) {
+		if inst == "do()" {
+			shouldSum = true
+			continue
+		} else if inst == "don't()" {
+			shouldSum = false
+			continue
+		}
+
+		if shouldSum {
+			sum += DoMultInstruction(inst)
+		}
+	}
+
+	return sum
 }

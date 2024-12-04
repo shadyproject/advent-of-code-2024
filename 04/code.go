f6968e5dbb8fddcbb647225b28427c2b1deca935
Chris Martin <shady@shadyproject.net>
2024-12-03 20:33:45 -0600

    structs methods and interfaces

---
+++ 04/code.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/herrnan/aoc-helper"
+)
+
+func main() {
+	h, _ := aoc.NewHelper(4, 2024)
+	input, _ := h.GetInput()
+
+	fmt.Printf("Day 4\n")
+	fmt.Printf("Part 1: %v\n", Part1(input))
+	fmt.Printf("Part 2: %v\n", Part2(input))
+}
+
+func Part1(input string) int {
+	return 0
+}
+
+func Part2(input string) int {
+	return 0
+}

2f05fbf24c805c35f61a506d494e2328aba0cf67
Chris Martin <shady@shadyproject.net>
2024-12-04 21:09:17 -0600

    Template file

---
+++ template.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/herrnan/aoc-helper"
+)
+
+const Day = 0
+const Year = 2024
+
+func main() {
+	h, _ := aoc.NewHelper(Day, Year)
+	input, _ := h.GetInput()
+
+	fmt.Printf("Day %v\n", Day)
+	fmt.Printf("Part 1: %v\n", Part1(input))
+	fmt.Printf("Part 2: %v\n", Part2(input))
+}
+
+func Part1(input string) int {
+	return 0
+}
+
+func Part2(input string) int {
+	return 0
+}
